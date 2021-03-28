ee335b9d39936bec34c8a845790c9f195b51c935
Jake Fried <samouri@users.noreply.github.com>
2021-03-28 17:51:51 -0400

    Upgrade organization.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path')
 
 module.exports = {
   target: 'webworker',
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   resolve: {
     alias: {
-      fs: path.resolve(__dirname, 'fs.js'),
-      terser: path.resolve(__dirname, 'terser.js'),
+      fs: path.resolve(__dirname, 'src', 'stub-modules', 'fs.js'),
+      terser: path.resolve(__dirname, 'src', 'stub-modules', 'terser.js'),
     },
   },
   // necessary for cloudflare worker to be secure since dev mode uses eval()
